2c356207e025ed7ad886affe1836ecb85f782d9c
poiuyreq0 <poiuyreq0@gmail.com>
2023-01-21 17:13:54 +0900

    project init

---
+++ app/src/main/java/poiuyreq0/numbercountingapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package poiuyreq0.numbercountingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
