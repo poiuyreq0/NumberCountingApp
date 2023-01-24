779136e076db168231715037c6d1ebf388a7341f
poiuyreq0 <poiuyreq0@gmail.com>
2023-01-24 17:58:59 +0900

    project init

--- app/src/main/java/poiuyreq0/numbercountingapp/MainActivity.kt
+++ app/src/main/java/poiuyreq0/numbercountingapp/MainActivity.kt
@@ -3,19 +3,23 @@ package poiuyreq0.numbercountingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
-import android.widget.TextView
+import poiuyreq0.numbercountingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var number: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val numberTextView = findViewById<TextView>(R.id.numberTextView)
-        val resetButton = findViewById<Button>(R.id.resetButton)
-        val plusButton = findViewById<Button>(R.id.plusButton)
-
-        var number: Int = 0
+        val numberTextView = binding.numberTextView
+        val resetButton = binding.resetButton
+        val plusButton = binding.plusButton
 
         resetButton.setOnClickListener {
             number = 0
@@ -28,5 +32,16 @@ class MainActivity : AppCompatActivity() {
             numberTextView.text = number.toString()
             Log.i("onClick", "plus number = $number")
         }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("number", number)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        number = savedInstanceState.getInt("number")
+        binding.numberTextView.text = number.toString()
+        super.onRestoreInstanceState(savedInstanceState)
     }
 }
